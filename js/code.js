0705bbec6f4a07e0cea510b756b32a8f3391a812
Marten <derapfelesser@aol.com>
2021-07-20 01:37:52 +0200

    Function playRound plays one round and delivers a result

--- js/code.js
+++ js/code.js
@@ -1,4 +1,5 @@
 let selectionComputer = "none";
+let selectionPlayer = "none";
 
 function computerPlay(){
     //returns a random number between 0 and 2
@@ -16,8 +17,39 @@ function computerPlay(){
             selectionComputer = "scissors";
             break;
     }
+}
 
-    return selectionComputer;
+function playRound(selectionPlayer, selectionComputer) {
+    //if selection from both players is the same, return draw
+    //if pc rock and player scissors, return pc won
+    //if pc paper and player rock, return pc won
+    //if pc scissor and player paper, return pc won
+    //else player won
+
+    let userIsWinner = undefined;
+
+    if (selectionPlayer === selectionComputer) {}
+    else if (
+            (selectionComputer === "rock" && selectionPlayer === "scissors") ||
+            (selectionComputer === "paper" && selectionPlayer === "rock") ||
+            (selectionComputer === "scissors" && selectionPlayer === "paper")
+    ) 
+    {
+            userIsWinner = true;
+    } else {
+            userIsWinner = false;
+    }
+
+    //returns a string, which states who won and because of what reason
+    switch (userIsWinner) {
+        case true:
+            return `You won! ${selectionPlayer} beats ${selectionComputer}`;
+        case false:
+            return `You lost! ${selectionComputer} beats ${selectionPlayer}`;
+        default:
+            return "Draw!";
+    }
 }
 
 selectionComputer = computerPlay();
+console.log(playRound("rock", "scissors"));
