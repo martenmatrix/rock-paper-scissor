a23779ef04f9fc870c3776546dc1d1e848e066b8
Marten <derapfelesser@aol.com>
2021-07-28 21:21:21 +0200

    Added logs and a scoreboard, logs are already working

--- js/code.js
+++ js/code.js
@@ -1,3 +1,10 @@
+function firstLetterUpperCase(string) {
+    let firstLetterUpperCase = (string.charAt(0)).toUpperCase();
+    let stringWithoutFirstLetter = string.slice(1);
+    
+    return firstLetterUpperCase + stringWithoutFirstLetter;
+}
+
 function computerPlay(){
     //returns a random number between 0 and 2
     let randomNumber = Math.floor(Math.random()*3);
@@ -19,7 +26,7 @@ function computerPlay(){
     return selectionComputer;
 }
 
-function playRound(selectionPlayer, selectionComputer) {
+function isUserWinner(selectionPlayer, selectionComputer) {
     //if selection from both players is the same, return draw
     //if pc rock and player scissors, return pc won
     //if pc paper and player rock, return pc won
@@ -44,12 +51,16 @@ function playRound(selectionPlayer, selectionComputer) {
 }
 
 function getWinnerMessage(userIsWinner, selectionPlayer, selectionComputer) {
+    selectionPlayer = firstLetterUpperCase(selectionPlayer);
+    selectionComputer = firstLetterUpperCase(selectionComputer);
+
+
     //returns a string, which states who won and because of what reason
     switch (userIsWinner) {
         case true:
-            return `You won! ${selectionPlayer} beats ${selectionComputer}`;
+            return `You won! ${selectionPlayer} beats ${selectionComputer}.`;
         case false:
-            return `You lost! ${selectionComputer} beats ${selectionPlayer}`;
+            return `You lost! ${selectionComputer} beats ${selectionPlayer}.`;
         default:
             return "Draw!";
     }
@@ -74,7 +85,16 @@ function hasUserWon (userPoint, computerPoint) {
 }
 
 function playRound(event) {
-    let playerSelection = event.currentTarget.getAttribute('data-selection');
+    let selectionPlayer = String((event.currentTarget).getAttribute('data-selection'));
+    let selectionComputer = computerPlay();
+
+    let userIsWinner = isUserWinner(selectionPlayer, selectionComputer);
+    let message = getWinnerMessage(userIsWinner, selectionPlayer, selectionComputer);
+    
+    let currentGameLog = document.createElement('p');
+    currentGameLog.textContent = message;
+    gameLogDiv.appendChild(currentGameLog);
+    
 }
 
 const buttonUserScissors = document.querySelector('#user-scissors');
@@ -82,5 +102,7 @@ const buttonUserRock = document.querySelector('#user-scissors');
 const buttonUserPaper = document.querySelector('#user-paper');
 
 const buttons = Array.from(document.querySelectorAll('#user-buttons > button'));
+
+const gameLogDiv = document.querySelector('#game-log');
 
 buttons.forEach(button => button.addEventListener('click', playRound));
